8b005b5556a6eed7d2fefec1ed770d54ef709174
nash <nash@iffy.me>
2024-05-01 10:19:20 +0800

    direct_keys

--- lua/taberm/core.lua
+++ lua/taberm/core.lua
@@ -1,4 +1,4 @@
-local u = require'taberm.utils'
+local u = require 'taberm.utils'
 
 local M = {}
 
@@ -6,8 +6,8 @@ local TAB_TERM = {}
 local TOGGLE_COUNT = {}
 
 
-function M.get (action, cmd, newtab)
-    return function (ctx)
+function M.get(action, cmd, newtab)
+    return function(ctx)
         local tab = vim.api.nvim_get_current_tabpage()
         local tot = TAB_TERM[tab]
         local cnt = vim.v.count1
@@ -29,7 +29,7 @@ function M.get (action, cmd, newtab)
         else
             if action then vim.api.nvim_command(action) end
             local name = '[' .. cnt .. ']' .. shell .. "://" .. "<b:terminal_job_pid>"
-            vim.fn.termopen(shell, {name = name})
+            vim.fn.termopen(shell, { name = name })
             --vim.api.nvim_command('silent tcd! .')
             if newtab then
                 tab = vim.api.nvim_get_current_tabpage()
@@ -42,14 +42,14 @@ function M.get (action, cmd, newtab)
         end
         local chan = vim.api.nvim_buf_get_var(TAB_TERM[tab][cnt], 'terminal_job_id')
         if ctx then
-            vim.api.nvim_chan_send(chan, ctx.args..'\n')
+            vim.api.nvim_chan_send(chan, ctx.args .. '\n')
         else
             vim.api.nvim_chan_send(chan, '')
         end
     end
 end
 
-function M.prepare (ctx)
+function M.prepare(ctx)
     vim.opt_local.number = false
     vim.opt_local.relativenumber = false
     vim.opt_local.spell = false
@@ -66,6 +66,26 @@ function M.prepare (ctx)
     -- :FIXME: curr_line == <term_last_line>
     if curr_line == last_line or curr_line < window_line then
         vim.api.nvim_command('startinsert')
+    end
+
+    local direct_keys = {
+        '<Enter>',
+        '<M-w>', '<M-d>', '<C-w>', '<C-d>',
+        '<C-a>', '<C-e>', '<M-a>', '<M-e>',
+        '<C-f>', '<C-b>', '<M-f>', '<M-b>',
+        '<C-n>', '<C-p>', '<M-n>', '<M-p>',
+    }
+    for _, k in ipairs(direct_keys) do
+        vim.keymap.set("n", k,
+            function() vim.api.nvim_input('i' .. k) end,
+            {
+                desc = "term: " .. k,
+                buffer = ctx.buf,
+                noremap = true,
+                silent = true,
+                nowait = true
+            }
+        )
     end
 end
 
@@ -95,7 +115,7 @@ function M.close_tab(tab)
 
     for _, u in pairs(unavailable) do
         for _, b in pairs(TAB_TERM[u]) do
-            vim.api.nvim_buf_delete(b, {force = true})
+            vim.api.nvim_buf_delete(b, { force = true })
         end
         TAB_TERM[u] = nil
     end
@@ -166,7 +186,7 @@ function M.toggle_taberm()
     --u.log{win_buf = win_buf, show = show, hide = hide, toggle = toggle, cterm = cterm, buf2win = buf2win}
 
     for w, _ in pairs(hide) do
-        vim.api.nvim_win_close(w, {force=true})
+        vim.api.nvim_win_close(w, { force = true })
     end
 
     local first = true
@@ -179,24 +199,22 @@ function M.toggle_taberm()
         end
         vim.api.nvim_win_set_buf(vim.api.nvim_get_current_win(), b)
     end
-
 end
 
 function M.debug()
     u.log { TAB_TERM = TAB_TERM, TOGGLE_IDX = TOGGLE_COUNT }
 end
 
-
-M.t  = M.get('tabnew', '', true)
-M.v  = M.get('rightbelow vnew', '')
-M.V  = M.get('botright vnew', '')
-M.c  = M.get('rightbelow new', '')
-M.C  = M.get('botright new', '')
-M.n  = M.get(nil, '')
+M.t = M.get('tabnew', '', true)
+M.v = M.get('rightbelow vnew', '')
+M.V = M.get('botright vnew', '')
+M.c = M.get('rightbelow new', '')
+M.C = M.get('botright new', '')
+M.n = M.get(nil, '')
 
 local layout_command = {
-    vertical = {'botright vnew', 'rightbelow new'},
-    horizontal = {'botright new', 'rightbelow vnew'}
+    vertical = { 'botright vnew', 'rightbelow new' },
+    horizontal = { 'botright new', 'rightbelow vnew' }
 }
 function M.setup(tbl)
     local conf = vim.tbl_deep_extend('force', require('taberm.config'), tbl or {})
@@ -204,10 +222,10 @@ function M.setup(tbl)
 
     M.layout_command = layout_command[conf.toggle_layout]
 
-    require'taberm.keymap'.config(conf.keymap)
+    require 'taberm.keymap'.config(conf.keymap)
 
     if conf.shell_integration then
-        require'taberm.shell'.setup()
+        require 'taberm.shell'.setup()
     end
 end
 
